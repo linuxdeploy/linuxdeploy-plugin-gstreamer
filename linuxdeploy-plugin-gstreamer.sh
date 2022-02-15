6488b7c8f3f057edab81cfbc79c5e5724f482c5f
zerobikappa <zerobikappa@gmail.com>
2022-02-15 20:37:10 +0800

    add some comment to explain the changes

--- linuxdeploy-plugin-gstreamer.sh
+++ linuxdeploy-plugin-gstreamer.sh
@@ -72,6 +72,7 @@ mkdir -p "$APPDIR"
 export GSTREAMER_VERSION="${GSTREAMER_VERSION:-1.0}"
 
 if [ "$GSTREAMER_PLUGINS_DIR" != "" ]; then
+    # remove additional slash"/", to prevent duplicate slash output in AppRun-hook
     plugins_dir="${GSTREAMER_PLUGINS_DIR%"/"}"
 else
     for i in "/lib" "/usr/lib"; do
@@ -81,13 +82,21 @@ else
 	    plugins_dir=$i/gstreamer-"$GSTREAMER_VERSION"
 	fi
     done
+    # if not found gstreamer plugin directory in /lib and /usr/lib,
+    # try to find it in /lib32 and /usr/lib32(if using 32-bit linux), or /lib64 and /usr/lib64(if using 64-bit linux)
     if [ ! -d "$plugins_dir" ]; then
+        # as far as I know, $(getconf LONG_BIT) indicates the default C compiling environment is 32 or 64 bit.
+	# it is usually the same with kernel, but not always. I only tested in archlinux distro.
+	# in this case, due to gstreamer is a C program,
+	# if $(getconf LONG_BIT) output "32", we assume the default gstreamer is 32-bit version.
 	for i in "/lib$(getconf LONG_BIT)" "/usr/lib$(getconf LONG_BIT)"; do
             [ -d "$i/gstreamer-$GSTREAMER_VERSION" ] && plugins_dir=$i/gstreamer-"$GSTREAMER_VERSION"
         done
     fi
 fi
 
+# /lib /usr/lib /lib32 /lib64 ... may be a symlink,
+# convert to the realpath to keep the same structure between AppDir and local system.
 plugins_dir=$(readlink -f "$plugins_dir")
 if [ ! -d "$plugins_dir" ]; then
     echo "Error: could not find plugins directory: $plugins_dir"
@@ -95,6 +104,7 @@ if [ ! -d "$plugins_dir" ]; then
 fi
 
 if [ "$GSTREAMER_HELPERS_DIR" != "" ]; then
+    # remove additional slash"/", to prevent duplicate slash output in AppRun-hook
     helpers_dir="${GSTREAMER_HELPERS_DIR%"/"}"
 else
     if [ -f "$plugins_dir/gstreamer-$GSTREAMER_VERSION/gst-plugin-scanner" ]; then
@@ -104,15 +114,27 @@ else
     else
         echo "Error: could not find gst-plugin-scanner"
         echo "Error: failed to locate gstreamer helpers directory automatically"
-        echo "Error: please consider to specify \$GSTREAMER_HELPERS_DIR"
+        echo "Error: please consider specifing \$GSTREAMER_HELPERS_DIR"
     fi
 fi
 
+# path may include symlink directory,
+# convert to the realpath to keep the same structure between AppDir and local system.
 helpers_dir=$(readlink -f "$helpers_dir")
 if [ ! -d "$helpers_dir" ]; then
     echo "Error: could not find helpers directory: $helpers_dir"
+    # in the original project, the process will exit if plugins_dir is not found,
+    # but still process if only the helpers is not found.
+    # I am not sure if this is correct, so I keep the same setting at this moment.
+    #exit 1
 fi
 
+# below three statements are the same thing:
+# "$APPDIR""$plugins_dir"
+# "$APPDIR"/${plugins_dir#/}
+# "$APPDIR"/${plugins_dir#"/"}
+# I choose the third one because it can let me easily know the directory is under AppDir.
+# same reason below when output the apprun-hook.
 plugins_target_dir="$APPDIR"/${plugins_dir#"/"}
 helpers_target_dir="$APPDIR"/${helpers_dir#"/"}
 
@@ -139,6 +161,7 @@ for i in "$plugins_target_dir"/*; do
     patchelf --set-rpath '$ORIGIN/..:$ORIGIN' "$i"
 done
 
+# $helpers_dir may be empty if helpers not found and $GSTREAMER_HELPERS_DIR not set in previous steps.
 if [ -n "$helpers_dir" ] && [ ! "$helpers_dir" = "$plugins_dir" ]; then
     mkdir -p "$helpers_target_dir"
 
